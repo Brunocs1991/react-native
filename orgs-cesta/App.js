7a70c92163538798785a667ef30d9a48d9b6cca6
Bruno <brunocosta.1991@hotmail.com>
2023-01-28 11:50:37 -0300

    configurando primeiro componente, e importando imagens

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alura!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
