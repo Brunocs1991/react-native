c3dfbdb12e6d7fddaa04563fe19a35e8eb524f2f
Bruno <brunocosta.1991@hotmail.com>
2023-01-28 14:21:59 -0300

    imagem estilos

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Cesta from './src/telas/cesta';
-
+import { SafeAreaView, StatusBar } from 'react-native';
+import Cesta from './src/telas/Cesta'
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
